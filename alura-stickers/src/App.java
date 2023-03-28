59b8860f00783767cc79c5024f99c46fa0f8ddc9
Douglas Vilas <douglas_vilass@hotmail.com>
2023-03-28 11:56:23 -0300

    Aula 1 - Consumindo uma API de Filmes em Java
    Continuação

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,20 +1,31 @@
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.security.Key;
+import java.util.List;
+import java.util.Map;
+import javax.swing.text.html.HTMLEditorKit.Parser;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
-        // Fazer uma conexão HTTP e buscar os tops 250 filmes
+    public static void main(String[] args) throws Exception {
+       // Fazer uma conexão HTTP e buscar os tops 250 filmes
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI endereço = URI.create(url);
+        URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(endereço).GET().build();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
 
         // Pegar só os dados que interessam (titulo, poster, classififcação) 
-
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
         // Exibir e manipular os dados
-
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));            
+        }
     }
 }
