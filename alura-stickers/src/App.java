4f37a9c7754a597e7b28c09856243c67c459a34c
Douglas Vilas <douglas_vilass@hotmail.com>
2023-03-30 08:23:28 -0300

    Aula 1 - Continuação

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -26,6 +26,9 @@ public class App {
         // Exibir e manipular os dados
         for (Map<String,String> filme : listaDeFilmes) {
             System.out.println(filme.get("title"));            
+            System.out.println(filme.get("image"));            
+            System.out.println(filme.get("imDbRating"));    
+            System.out.println();        
         }
     }
 }
