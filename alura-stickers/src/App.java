23fb194474d69d196a42c0070454f9f221704e39
Douglas Vilas <douglas_vilass@hotmail.com>
2023-04-01 22:05:25 -0300

    Continuação da Aula 2 - Gerando Figurinhas para WhatsApp

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,17 +1,17 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.security.Key;
 import java.util.List;
 import java.util.Map;
-import javax.swing.text.html.HTMLEditorKit.Parser;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-       // Fazer uma conexão HTTP e buscar os tops 250 filmes
+        // Fazer uma conexão HTTP e buscar os tops 250 filmes
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -19,16 +19,24 @@ public class App {
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
-        // Pegar só os dados que interessam (titulo, poster, classififcação) 
+        // Pegar só os dados que interessam (titulo, poster, classififcação)
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        
+
         // Exibir e manipular os dados
-        for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));            
-            System.out.println(filme.get("image"));            
-            System.out.println(filme.get("imDbRating"));    
-            System.out.println();        
+        for (Map<String, String> filme : listaDeFilmes) {
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo.replace(":", "-") + ".png";
+
+            var geradora = new GeradoraDeFigurinhas();
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+            System.out.println();
         }
     }
 }
--- alura-stickers/src/GeradoraDeFigurinhas.java
+++ alura-stickers/src/GeradoraDeFigurinhas.java
@@ -1,19 +1,22 @@
-import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.awt.Color;
-import java.awt.Font;
+import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
 public class GeradoraDeFigurinhas {
 
-    public void cria() throws Exception {
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
 
         // Leitura da imagem
-        BufferedImage imagemOriginal = ImageIO.read(new File(
-                "C:\\Users\\dougl\\OneDrive\\Documentos\\MeusProjetos\\Imersao-Java\\alura-stickers\\entrada\\filme.jpg"));
+        // InputStream inputStream = new FileInputStream(new
+        // File("C:\\Users\\dougl\\OneDrive\\Documentos\\MeusProjetos\\Imersao-Java\\alura-stickers\\entrada\\filme.jpg"));
+        // InputStream inputStream = new
+        // URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
         // Criar uma nova imagem em memória com transparência e com tamanho novo
         int largura = imagemOriginal.getWidth();
@@ -34,13 +37,6 @@ public class GeradoraDeFigurinhas {
         graphics.drawString("TOPZERA", 100, novaAltura - 100);
 
         // Escrever a nova imagem em um arquivo
-        ImageIO.write(novaImagem, "png", new File(
-                "C:\\Users\\dougl\\OneDrive\\Documentos\\MeusProjetos\\Imersao-Java\\alura-stickers\\saida\\figurinha.png"));
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
     }
-
-    public static void main(String[] args) throws Exception {
-        var geradora = new GeradoraDeFigurinhas();
-        geradora.cria();
-    }
-
 }
