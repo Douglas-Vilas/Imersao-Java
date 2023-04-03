a797e6f061277f89e91e369321c35bada097ed77
Douglas Vilas <douglas_vilass@hotmail.com>
2023-04-02 21:17:46 -0300

    Continuação

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -12,8 +12,11 @@ public class App {
         //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         //ExtratorDeConteudo extrator = new ExtratorDeConteudoIMDB();
 
-        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
-        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+        //String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        //ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+
+        String url = "http://localhost:8081/linguagens";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoIMDB();
         
         var http = new ClienteHttp();
         String json = http.buscaDados(url); //json é o body da classe ClienteHttp        
